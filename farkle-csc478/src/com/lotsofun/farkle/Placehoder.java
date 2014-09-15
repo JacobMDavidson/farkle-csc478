125f46f3aa9cefa6ed082f857b0c449e2ec9597a
Jacob Davidson <jacobdavidson79@gmail.com>
2014-09-14 20:10:26 -0500

    Jake's first commit to test Git setup

--- farkle-csc478/src/com/lotsofun/farkle/Placehoder.java
+++ farkle-csc478/src/com/lotsofun/farkle/Placehoder.java
@@ -1,5 +1,6 @@
 package com.lotsofun.farkle;
 
+// This is a test comment from Jacob Davidson to ensure proper Git setup
 public class Placehoder {
 	public static void main(String[] args) {
 		System.out.println("This is random text to create a project.");
