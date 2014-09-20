f4ff64db664eec5a97700ebfaa6a0106d91f5f14
Brant Mullinix <brant.mullinix@gmail.com>
2014-09-20 10:53:54 -0400

    Added a line to test Git setup.

--- farkle-csc478/src/com/lotsofun/farkle/Placehoder.java
+++ farkle-csc478/src/com/lotsofun/farkle/Placehoder.java
@@ -1,8 +1,10 @@
 package com.lotsofun.farkle;
 
 // This is a test comment from Jacob Davidson to ensure proper Git setup
+//     also a test from BrMu to ensure proper Git setup
 public class Placehoder {
 	public static void main(String[] args) {
 		System.out.println("This is random text to create a project.");
+		System.out.println("This is a text to verfiy Brant's git setup.");
 	}
 }
